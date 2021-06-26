f371dca5cae4e82d18dfdd5b6e11814081d4ad8a
yekyung2 <skybluee2014@gmail.com>
2021-06-26 14:15:28 +0900

    save crawling data as csv file

---
+++ save.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def save_to_file(jobs):
+    file = open("jobs.csv", encoding="utf-8", mode="w")
+    writer = csv.writer(file)
+    writer.writerow(["title", "company", "location", "link"])
+    for job in jobs:
+        writer.writerow(list(job.values()))
+    file.close()
+    return
